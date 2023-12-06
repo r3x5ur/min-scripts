0b0fb4a611aadd922e919d83e7d7ffe89ea65d6e
r3x5ur <64947085+r3x5ur@users.noreply.github.com>
2023-12-06 09:05:40 +0800

    Create eruda.js

---
+++ js/eruda.js
@@ -0,0 +1,3 @@
+// 快速开启一个三方devtools, eruda
+/**chrome 标签版本**/ 
+javascript:(function(){var n;(n=document.createElement("script")).src="https://cdn.jsdelivr.net/npm/eruda",document.body.append(n),n.onload=function(){eruda.init()}})()
