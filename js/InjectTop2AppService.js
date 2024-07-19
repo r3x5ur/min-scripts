158d38098be9552f3d8ba9701a9d75b7c4fd8a30
r3x5ur <64947085+r3x5ur@users.noreply.github.com>
2024-07-19 11:14:57 +0800

    Update InjectTop2AppService.js

--- js/InjectTop2AppService.js
+++ js/InjectTop2AppService.js
@@ -1,9 +1,15 @@
-// 注入到 page-frame 处
 setTimeout(() => {
+  window.top.loadJS = url => {
+    const document = window.top.document
+    const script = document.createElement('script')
+    script.src = url
+    document.head.appendChild(script)
+  }
   for (let i = 0; i < 100; ++i) {
     let f = window.top.frames[i];
     if (!f) break;
     console.dir(f);
     if (!f.top) f.top = window.top;
   }
+  window.top.loadJS('http://127.0.0.1:60010/inject.js')
 }, 1000);
