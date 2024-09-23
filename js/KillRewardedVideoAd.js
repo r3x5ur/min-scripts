0e2e7474667aecbfa6cadcaf7de27e76c655c79a
r3x5ur <64947085+r3x5ur@users.noreply.github.com>
2024-09-23 15:53:23 +0800

    Update KillRewardedVideoAd.js

--- js/KillRewardedVideoAd.js
+++ js/KillRewardedVideoAd.js
@@ -1,17 +1,16 @@
 // 注入到程序入口处，即可生效， 究极无敌版（通杀所有小游戏）
-const ad = wx.createRewardedVideoAd()
-ad.onLoad = fn => {
-  ad.__onLoad = fn
+function df(o, k, v) {Object.defineProperty(o, k, {value: v,writable: true,enumerable: true,configurable: true})}
+function noop(){}
+const createRewardedVideoAd = wx.createRewardedVideoAd
+wx.createRewardedVideoAd = function(option) {
+	if (option) delete option.multiton
+	return createRewardedVideoAd.apply(this, arguments)
 }
-ad.onClose = fn => {
-  ad.__onClose = fn
-}
-ad.load = () => {
-  ad.__onLoad && ad.__onLoad()
-  return Promise.resolve()
-}
-ad.show = () => {
-  ad.__onClose && ad.__onClose({isEnded: true})
-  return Promise.resolve()
-}
-ad.destroy = Boolean
+const iad = wx.createInterstitialAd()
+const rad = wx.createRewardedVideoAd()
+df(iad, 'load', noop)
+df(rad, 'load', () => rad.__onLoad && rad.__onLoad(), Promise.resolve())
+df(rad, 'show', () => rad.__onClose && rad.__onClose({isEnded: true}), Promise.resolve())
+df(rad, 'destroy', noop)
+df(rad, 'onLoad', fn => rad.__onLoad = fn)
+df(rad, 'onClose', fn => rad.__onClose = fn)
